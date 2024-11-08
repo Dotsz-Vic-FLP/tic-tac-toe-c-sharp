84d46b315d268f3544dff5d92012c9670dfbfbc6
Victor Felipe Jr <144490375+Dotsz-Vic-FLP@users.noreply.github.com>
2024-11-08 15:17:57 -0500

    code session 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,135 +1,206 @@
-﻿
+﻿// https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-6.0
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+// https://learn.microsoft.com/en-us/dotnet/csharp/how-to/parse-strings-using-split
+// https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.select?view=net-8.0
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 App app = new App();
 app.Run();
 
 
 internal class App
-{ // class to run the application
+{
+    // class to run the application
     public void Run()
     {
 
-        Util.Print("Tic-Tac-Toe Game:", ConsoleColor.DarkBlue);
+        Util.Print("Tic-Tac-Toe Game:\n", ConsoleColor.DarkBlue);
 
         // call the function to show the board
-
-        List<List<string>> board = InitializeList();
+        List<String> row = InitializeListString();
+        List<List<string>> board = InitializeGameBoard(row);
 
         int turn = 1;
         while (true)
         {
 
-            Util.GenerateGrid(board);
-            
-            if(turn%2 == 0)
+            Util.Print(UI.GenerateBoard(board));
+
+            if (turn % 2 == 0)
             {
-                Util.Print("Player 2's turn (o)", ConsoleColor.DarkGreen);
-                
+                Util.Print("Player 2's turn (o)\n Valid format(\'x,y\')", ConsoleColor.DarkGreen);
+
+                ValidateInput("O", board);
                 // call get input function
                 // update the board
                 // check for win condition
                 // check for draw condition
                 // increment turn
-                
+
             }
             else
             {
-                Util.Print("Player 1's turn (x) ", ConsoleColor.DarkGreen);
-                // call get input function
+                Util.Print("Player 1's turn (x)\n", ConsoleColor.DarkMagenta);
+                ValidateInput("X", board);
                 // update the board
                 // check for win condition
                 // check for draw condition
                 // increment turn
-                
+
             }
-            
-            
+
+            if (turn > 9)
+            {
+                Util.Print("Game Over! It's a draw!", ConsoleColor.DarkRed);
+                break;
+            }
+
             // exit condition
-            // System.Environment.Exit(0);
+            turn++;
         }
 
     }
-
-    
-    private static List<List<string>> InitializeList ()
-    {
-        List<string> row = [];
-        List<List<string>> tikTakToe = [[]];
-        for(int i = 0; i <= 3; i++ )
-        {
-            row.Add(UI.BLANK);
-        }
-        for(int i = 0; i <= 3; i++ )
-        {
-            tikTakToe.Add(row);
-        }
-        return tikTakToe;
-    }
-
-}
-
-
 
 /**
- * UI class
- * 
- * This class contains all the UI elements
+ * Gets the input on which coordinate the player would like to choose
+ *
  */
-class UI
-{
-    /**
-     * Generates a bounding line
-     * <param name="length"> The length of the line </param>
-     */
-    public static string DividerLine(int length)
-    {
 
-        string result = string.Empty;
-        for (int i = 0; i <= length; i++)
+
+    public List<int> ValidateInput(string input, List<List<string>> gameBoard)
+    {
+        List<int> coordinates = new List<int>();
+        char[] delimiters = { ' ', ',' , ':'};
+
+        try
+        { // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.select?view=net-8.0
+            // check if the input is valid
+            coordinates = Console.ReadLine().Split(delimiters).Select(Int32.Parse).ToList();
+            if( coordinates.Count == 2 && coordinates[0] < 3 && coordinates[1] > 3 )
+            {
+                gameBoard[coordinates[0]][coordinates[1]] = input;
+
+            }
+
+
+
+        }
+        catch (Exception e)
         {
-            result += DIVIDER_UNIT;
+            Util.Print("Invalid input. Please enter a valid input.\n", ConsoleColor.DarkRed);
+
         }
 
-        return result;
+        return null; // temporary
     }
-    public static readonly string DIVIDER_UNIT = "+";
-    public static readonly string BLANK = "_"; //Readonly string what is this??? // readonly means final; the value doesn't change
+
+
+
+    private static List<List<string>> InitializeGameBoard (List<string> row)
+    {
+        List<List<string>> gameBoard = [];
+        for(int i = 0; i < 3; i++ )
+        {
+            gameBoard.Add(row); // add 3 rows to the gameBoard
+        }
+        return gameBoard;
+    }
+
+    private static List<string> InitializeListString()
+    {
+        List<string> row = []; // initialize an empty list for row
+        for(int i = 0; i < 3; i++ )
+        {
+            row.Add(UI.BLANK);// add 3 blanks to the row
+        }
+
+        return row;
+    }
+
 }
+
+
+
+
+
+
 
 /**
  * Util class
- * 
+ *
  * This class contains all the utility functions
  */
 class Util
 {
+
+
+    /**
+     * Prints a message
+     * <param name="message"> The message to be printed </param>
+     * <param name="color"> The color of the message </param>
+     */
     public static void Print(string message, ConsoleColor color)
     {
         Console.ForegroundColor = color;
         Console.Write(message);
     }
-    
+
+    /**
+     * Prints a message
+     * <param name="message"> The message to be printed </param>
+     */
     public static void Print(string message)
     {
         Console.ForegroundColor = ConsoleColor.Black;
         Console.Write(message);
     }
-
-    public static string GenerateGrid(List<List<string>> grid)
-    {
-        Print(UI.DividerLine(grid.Count));
-        foreach (List<string> row in grid)
-        {
-            foreach (string item in row)
-            {
-                Print(item);
-            }
-        }
-        
-        // return what?
-        return "";
-    }
 }
+
+/**
+ * UI class
+ *
+ * This class contains all the UI elements
+*/
+class UI
+    {
+        /**
+         * idk what it did before but now it Grabs the entire Board and turns it into a Visual so that the user
+         * may see what posistions are open and closed
+         * <param name=gameBoard> all of the possible positions for the player to use</param>
+         */
+        public static string GenerateBoard(List<List<string>> gameBoard)
+        {
+            string result = "#######\n";
+            foreach (var row in gameBoard)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == 2)
+                    {
+                        result += $"{DIVIDER_UNIT}{row[i]}{DIVIDER_UNIT}";
+                    }
+                    else
+                    {
+                        result += $"{DIVIDER_UNIT}{row[i]}";
+                    }
+                }
+
+                result += "\n";
+            }
+
+            result += "#######";
+            return result;
+        }
+
+        // Constants
+        private static readonly string DIVIDER_UNIT = "#";
+        public static readonly string BLANK = "_";
+}
+
+
+
+
 
 
 
